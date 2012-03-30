a2122b1200a364ea79db3cb1860aa17b653015a5
Nicolas Luck <nicolas@lucksus.eu>
2012-03-30 16:16:53 +0200

    Tabs -> spaces.

--- src/PJobFile/PJobFile.h
+++ src/PJobFile/PJobFile.h
@@ -15,71 +15,71 @@ using namespace std;
 class PJobFile : public QObject{
 Q_OBJECT
 public:
-	PJobFile(QString pjobFile);
-	~PJobFile();
-	void addResource(QString path);
-	void addResource(const QByteArray& content, const QString& internal_file_name);
-        QStringList runDirectoryEntries() const;
-	QString latestRunDirectory() const;
-	QString pjobFile() const;
-	QString mainPscript() const;
-	void setMainPscript(QString script);
-	PJobFileFormat* getPJobFile();
+    PJobFile(QString pjobFile);
+    ~PJobFile();
+    void addResource(QString path);
+    void addResource(const QByteArray& content, const QString& internal_file_name);
+    QStringList runDirectoryEntries() const;
+    QString latestRunDirectory() const;
+    QString pjobFile() const;
+    QString mainPscript() const;
+    void setMainPscript(QString script);
+    PJobFileFormat* getPJobFile();
 
-	QList<PJobFileParameterDefinition> parameterDefinitions() const;
-	void addParameterDefinition(const PJobFileParameterDefinition&);
-	void removeParameterDefinition(QString parameterName);
-	void writeParameterDefinitions(QList<PJobFileParameterDefinition>);
+    QList<PJobFileParameterDefinition> parameterDefinitions() const;
+    void addParameterDefinition(const PJobFileParameterDefinition&);
+    void removeParameterDefinition(QString parameterName);
+    void writeParameterDefinitions(QList<PJobFileParameterDefinition>);
 
-	QList<PJobFileParameter> readParameterCombinationForRun(QString run);
-	void writeParameterCombinationForRun(QList<PJobFileParameter>, QString run);
+    QList<PJobFileParameter> readParameterCombinationForRun(QString run);
+    void writeParameterCombinationForRun(QList<PJobFileParameter>, QString run);
 
-	QList<PJobResultFile> readResultDefinitions();
-	void writeResultDefinitions(QList<PJobResultFile>);
+    QList<PJobResultFile> readResultDefinitions();
+    void writeResultDefinitions(QList<PJobResultFile>);
 
-        QList<PJobFileApplication> applications() const;
-        PJobFileApplication applicationByName(QString name) const;
-        void addApplication(const PJobFileApplication&);
-        void removeApplication(const QString& name);
-        void writeApplications(const QList<PJobFileApplication>&);
-        void renameApplication(const QString& old_name, const QString& new_name);
-        QString defaultApplication();
-        void setDefaultApplication(const QString& name);
+    QList<PJobFileApplication> applications() const;
+    PJobFileApplication applicationByName(QString name) const;
+    void addApplication(const PJobFileApplication&);
+    void removeApplication(const QString& name);
+    void writeApplications(const QList<PJobFileApplication>&);
+    void renameApplication(const QString& old_name, const QString& new_name);
+    QString defaultApplication();
+    void setDefaultApplication(const QString& name);
 
-	void save();
-	void setSaveAutomatically(bool);
-	
-	void copyWithoutRuns(QString path);
-	void mergeRunsFrom(const PJobFile&);
-	QHash< QHash<QString,double>, QHash<QString,double> > getResultsForRun(QString run);
+    void save();
+    void setSaveAutomatically(bool);
 
-	void checkIfRunIsProperlyFinished(QString run);
+    void copyWithoutRuns(QString path);
+    void mergeRunsFrom(const PJobFile&);
+    QHash< QHash<QString,double>, QHash<QString,double> > getResultsForRun(QString run);
 
-	void remove_all_runs();
+    void checkIfRunIsProperlyFinished(QString run);
+
+    void remove_all_runs();
 
 public slots:
-	void abort_progress(const QString &);
+    void abort_progress(const QString &);
 
 signals:
-	void changed();
-	void progress(QString message, unsigned int percent);
+    void changed();
+    void progress(QString message, unsigned int percent);
 
 protected:
-	PJobFileFormat* m_data;
-	QString m_pjobFile;
-	bool m_saveAutomatically;
+    PJobFileFormat* m_data;
+    QString m_pjobFile;
+    bool m_saveAutomatically;
 
-	QList<PJobFileParameter> makeParameterCombinationValid(const QList<PJobFileParameter>&) const;
+    QList<PJobFileParameter> makeParameterCombinationValid(const QList<PJobFileParameter>&) const;
 
 private:
-	void create();
-	
-	//funktionieren z.Z. nicht!
-	QList< QHash< QString, double > > readResultFilePHOTOSS_CSV(QString resultFile);
+    void create();
+
+    //funktionieren z.Z. nicht!
+    QList< QHash< QString, double > > readResultFilePHOTOSS_CSV(QString resultFile);
     QHash< QHash<QString,double>, QHash<QString,double> >  readResultsPHOTOSS_CSV(QString run);
-	static QList< QHash< QString, double > > resultsFromFile(istream& input);
-	static QStringList readHeader(istream& file);
-	static QHash<QString,double> readValues(istream& input, QStringList parameters);
-	static pair< QStringList, QStringList > tellParametersAndResultsApart(QStringList header, QStringList knownParameters);
-	QSet<QString> m_progresses_to_abort;
+    static QList< QHash< QString, double > > resultsFromFile(istream& input);
+    static QStringList readHeader(istream& file);
+    static QHash<QString,double> readValues(istream& input, QStringList parameters);
+    static pair< QStringList, QStringList > tellParametersAndResultsApart(QStringList header, QStringList knownParameters);
+    QSet<QString> m_progresses_to_abort;
 };
